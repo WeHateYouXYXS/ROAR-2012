8aea5961aed136abe4096a1f08b5b4025886f147
vvita <98233732+WeHateYouXYXS@users.noreply.github.com>
2023-01-14 23:03:03 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,61 @@
+
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="polygon-csrf" content="false"> 
+    <link rel='shortcut icon' type='image/x-icon' href='img/rbx2log.png' />
+    <meta name="theme-color" content="#eb4034">
+    <meta property="og:title" content="ROAR">
+    <meta property="og:site_name" content="ROAR">
+    <meta property="og:url" content="https://rccservice.xyz">
+    <meta property="og:description" content="we do what roar does">
+    <meta property="og:type" content="Website">
+    <meta property="og:image" content="/minilogo.jpeg">
+    </head>
+    <title>Free Stuff at Rccservice.xyz</title>
+<link id="ctl00_ctl00_Imports" rel="stylesheet" type="text/css" href="https://rccservice.xyz/CSS/Base/CSS/FunnyCSS.css">
+             <div id="Container">
+            
+        
+        <div id="SmallHeaderContainer">
+        
+            
+                <div id="Banner">
+                    <div class="BannerCenterContainer">
+                        <a href="/" class="btn-logo"></a>
+                      
+          <br>
+                      <br>
+                      </div>
+                  </div>
+          </div>
+               </div>
+    <div id="Header" class="iframeHeader" style="position:static;width:100%;">
+            
+
+<div id="Nav" class="Navigation">
+    <ul id="ctl00_cphBanner_cphBannerHeader_Menu_MenuUL">
+        
+                <li>
+                    <a href="/My/Home.php" style="" ref="nav-myroblox"><h2>My ROAR</h2></a>
+                </li>
+            
+        
+        <li class="gamesLink"><a id="hlGames" href="/" style="" title="Games" ref="nav-games"><h2>Games</h2></a> </li>
+        <li><a id="hlCatalog" href="/" style="" title="Catalog" ref="nav-catalog"><h2>Catalog</h2></a></li>
+        <li><a id="hlBrowse" href="/Users" style="" title="People" ref="nav-people"><h2>People</h2></a></li>
+        <li><a id="hlForum" onclick="" href="/" style="" title="Forum" ref="nav-forum"><h2>Forum</h2></a></li>
+        <li><a id="hlNews" href="/" title="Blog" ref="nav-news"><h2>Blog</h2></a></li>
+        <li><a id="hlHelp" href="/" style="" title="Help" ref="nav-help"><h2>Help</h2></a></li>
+    </ul>
+</div>
+
+        </div>
+        </div>
+<br>
+    <br>
+    <br>
+ <br>
+
+    
