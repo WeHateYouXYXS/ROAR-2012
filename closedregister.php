cef15c9d3f7194cf7103068ae9eb0943edf0fade
vvita <98233732+WeHateYouXYXS@users.noreply.github.com>
2023-01-15 01:42:09 +0000

    Add files via upload

---
+++ closedregister.php
@@ -0,0 +1,26 @@
+<?php include 'header.php'?>
+
+<center>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  Sign up is currently disabled. Come back later!!
+  
+  <br>
+Users registered: <b>150+</b>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <hr>
+  <h2>ROAR 2021-2022</h2>
+  <hr>
